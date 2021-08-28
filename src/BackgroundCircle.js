272d8769ecdde682a708b030b5d88770d295dd5a
Nafiul Nipu <nafiulnipu.2013@gmail.com>
2021-08-28 11:55:04 -0500

    lets make a face

---
+++ src/BackgroundCircle.js
@@ -0,0 +1,9 @@
+export const BackgroundCircle = ({radius, strokeWidth}) => (
+    <circle
+      r={radius}
+      fill="yellow"
+      stroke="black"
+      strokeWidth={strokeWidth}
+    />
+  );
+  
